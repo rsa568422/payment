9b46b4596e507e37afe6cd57f6e63445bff47652
Roberto Sánchez <rsa568422@gmail.com>
2024-09-28 13:36:17 +0200

    Se agrega método PaymentMapper::toModel, mapper RecordMapper e implementación de los métodos de PaymentRepositoryAdapter haciendo uso de PaymentJpaRepository, PaymentMapper y RecordMapper

--- payment-infrastructure/src/main/java/com/ntt/payment/infrastructure/adapter/PaymentRepositoryAdapter.java
+++ payment-infrastructure/src/main/java/com/ntt/payment/infrastructure/adapter/PaymentRepositoryAdapter.java
@@ -3,21 +3,32 @@ package com.ntt.payment.infrastructure.adapter;
 import com.ntt.payment.domain.model.Payment;
 import com.ntt.payment.domain.model.Record;
 import com.ntt.payment.domain.repository.PaymentRepository;
+import com.ntt.payment.infrastructure.mapper.PaymentMapper;
+import com.ntt.payment.infrastructure.mapper.RecordMapper;
+import com.ntt.payment.infrastructure.repository.PaymentJpaRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
 @Repository
+@RequiredArgsConstructor
 public class PaymentRepositoryAdapter implements PaymentRepository {
+
+    private final PaymentJpaRepository paymentJpaRepository;
+
+    private final PaymentMapper paymentMapper;
+
+    private final RecordMapper recordMapper;
 
     @Override
     public List<Record> findAll() {
-        return List.of();
+        return recordMapper.toModels(paymentJpaRepository.findAll());
     }
 
     @Override
     public UUID save(Payment payment) {
-        return UUID.randomUUID();
+        return paymentJpaRepository.save(paymentMapper.toEntity(payment)).getId();
     }
 }
---
+++ payment-infrastructure/src/main/java/com/ntt/payment/infrastructure/mapper/RecordMapper.java
@@ -0,0 +1,17 @@
+package com.ntt.payment.infrastructure.mapper;
+
+import com.ntt.payment.domain.model.Record;
+import com.ntt.payment.infrastructure.entity.PaymentEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", uses = {PaymentMapper.class})
+public interface RecordMapper {
+
+    @Mapping(target = "payment", source = "payment")
+    Record toModel(PaymentEntity payment);
+
+    List<Record> toModels(List<PaymentEntity> payments);
+}
